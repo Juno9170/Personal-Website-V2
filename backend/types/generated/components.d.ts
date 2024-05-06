49367493e3b64a5fa0adc3c5eb79ee48fe72fdbd
Juno9170 <ryanzhuturtle@gmail.com>
2024-05-06 13:46:14 -0400

    graphQl

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SeoMetaData extends Schema.Component {
+  collectionName: 'components_seo_meta_data';
+  info: {
+    displayName: 'Meta Data';
+    icon: 'filter';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.Text;
+    metaImage: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'seo.meta-data': SeoMetaData;
+    }
+  }
 }
